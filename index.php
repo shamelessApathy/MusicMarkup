5a5599167733df291e060074f46cb60ee7bf79c8
shamelessApathy <bcm811@gmail.com>
2019-12-21 13:50:33 -0700

    hot-cross-buns

--- index.php
+++ index.php
@@ -1,19 +1,73 @@
 <?php
 
-echo "Music Markup v1.0";
-?>
+                                                                           ?>
 <head>
     <link href="css/treble.css" rel="stylesheet" type="text/css" />
 </head>
+<h1>MusicMarkup v1.0</h1>
+<h2>This is a quick markup of Hot Cross Buns, a grade school classic</h2>
 <div class="row">
     <div class="staff">
         <div class="treble"></div>
         <div class="time-signature-4-4"></div>
         <div class="measure">
-            <div class="quarter-note  a"></div>
-            <div class="quarter-note  b"></div>
-            <div class="quarter-note-line  low-c"></div>
-            <div class="quarter-note  low-d"></div>
+            <div class="note-wrap b">
+                <div class="quarter-note down"></div>
+            </div>
+            <div class="note-wrap a">
+                <div class="quarter-note down"></div>
+            </div>
+            <div class="note-wrap g">
+                <div class="half-note"></div>
+            </div>
         </div>
+        <div class="measure">
+            <div class="note-wrap b">
+                <div class="quarter-note down"></div>
+            </div>
+            <div class="note-wrap a">
+                <div class="quarter-note down"></div>
+            </div>
+            <div class="note-wrap g">
+                <div class="half-note"></div>
+            </div>
+        </div>
+        <div class="measure">
+            <div class="note-wrap g">
+                <div class="eighth-note"></div>
+            </div>
+            <div class="note-wrap g">
+                <div class="eighth-note"></div>
+            </div>
+            <div class="note-wrap g">
+                <div class="eighth-note"></div>
+            </div>
+            <div class="note-wrap g" style="margin-right:30px;">
+                <div class="eighth-note"></div>
+            </div>
+            <div class="note-wrap a">
+                <div class="eighth-note down"></div>
+            </div>
+            <div class="note-wrap a">
+                <div class="eighth-note down"></div>
+            </div>
+            <div class="note-wrap a">
+                <div class="eighth-note down"></div>
+            </div>
+            <div class="note-wrap a">
+                <div class="eighth-note down"></div>
+            </div>
+        </div>
+         <div class="measure">
+             <div class="note-wrap b">
+                 <div class="quarter-note down"></div>
+             </div>
+             <div class="note-wrap a">
+                 <div class="quarter-note down"></div>
+             </div>
+             <div class="note-wrap g">
+                 <div class="half-note"></div>
+             </div>
+         </div>
     </div>
 </div>
